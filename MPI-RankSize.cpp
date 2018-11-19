367a411b4a972d38e64c2c6964fecea6d8883921
PyCharm <pagarwal@buffalo.edu>
2018-11-19 14:21:35 +0000

    MPI Commit

---
+++ MPI-RankSize.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <mpi.h>
+using namespace std;
+
+int main(int argc, char* argv[]) 
+{
+  MPI_Init(&argc, &argv);
+  int rank, size;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  cout <<"The rank of the processor is: "<< rank << "\n";
+  cout <<"The size is: "<< size << "\n";
+  return MPI_Finalize();
+}
